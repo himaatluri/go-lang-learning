5765be058bfc2d6366a4b2a20c669deb166faf41
hima atluri <himain8977@gmail.com>
2020-09-30 23:28:33 -0500

    Saving stages

---
+++ helloWorld/sample.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	println("Hello\n")
+	printer()
+
+	variabler()
+}
+
+func printer() {
+	println("This is printer function\n")
+}
+
+func variabler() {
+	var name string = "Hima"
+
+	println(name)
+}
