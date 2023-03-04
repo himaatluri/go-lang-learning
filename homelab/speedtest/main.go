31183ae5c2a223c8d9bce2f0e85bdf2b738a8b32
haops <himasagar.atluri@gmail.com>
2023-03-04 14:49:02 -0600

    speedtest

--- homelab/speedtest/main.go
+++ homelab/speedtest/main.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
 )
+
+type testReport struct {
+	Latency  string
+	Download string
+	Upload   string
+	// Jitter   string
+}
 
 func main() {
 	fmt.Println("Run speed test")
@@ -13,6 +21,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//Latency
+	lat, _ := regexp.Compile("Latency:\\s+(.*?)s")
+	dwn, _ := regexp.Compile("Download:\\s+(.*?)s")
+	upl, _ := regexp.Compile("Upload:\\s+(.*?)s")
+	// jit, _ := regexp.Compile("Latency:.*?jitter:\\s+(.*?)ms")
 
-	fmt.Println(string(op))
+	Latency := lat.FindString(string(op))
+	Download := dwn.FindString(string(op))
+	Upload := upl.FindString(string(op))
+	// Jitter := jit.FindString(string(op))
+
+	out := fmt.Sprintf("%s %s %s", Latency, Download, Upload)
+
+	fmt.Println(out)
+
+	rpt := testReport{Latency: Latency}
+	rpt.Download = Download
+	rpt.Upload = Upload
+	// rpt.Jitter = Jitter
+
+	fmt.Println(rpt)
 }
